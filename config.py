ab771f711f15d2cf602cc532e6472e7f17703dbe
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-01 07:37:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Your Info that Jarvis AI 4.0.1 needs
-NAME = "itzzzashu" # Your first name that Jarvis can call you
-CLASS = 11 # Your class no. without section
-AGE = 16 # Your current age without 'th' can be found by (Current Year - Your Date of birth)
+NAME = "xyz" # Your first name that Jarvis can call you
+CLASS =  # Your class no. without section
+AGE =  # Your current age without 'th' can be found by (Current Year - Your Date of birth)
 
 # TIME FORMAT
 TFT = 12 # if you want 12 hours format, enter 12 in it else 24.
@@ -10,15 +10,15 @@ CNT = 'IST' # IST or UTC.
 
 # Directories
 #    Copy paste your music directory path in music_dir.
-music_dir = 'E:\YBDB Backup Folder\Musics\Punjabi\Jass-Manak'
+music_dir = ''
 #    Copy paste your photos directory path in pics_dir.
-pics_dir = 'E:\YBDB Backup Folder\Photoes\MY ALBUM'
+pics_dir = ''
 #    Copy paste your videos directory path in video_dir.
-video_dir = 'E:\YBDB Backup Folder\Photoes\Dream Vids'
+video_dir = ''
 #    Copy paste your jarvis.py directory path in jarvis_path.
-jarvis_path = 'E:\YBDB Backup Folder\Documents\PYTHON FILES\Jarvis-4.0.1'
+jarvis_path = ''
 #    Copy paste your hidden directory path in hidden_dir.
-hidden_dir = 'E:\YBDB Backup Folder\Photoes\Hidden-dir'
+hidden_dir = ''
 #   Secret Codes to unlock hidden_dir.
 xsearch=['umm','hn']
 
